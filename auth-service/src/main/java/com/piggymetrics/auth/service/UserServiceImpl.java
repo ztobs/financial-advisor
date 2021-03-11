af4320ab304c5a486136761d8f4f1b6625a17d6a
Tobi Lukan <ztobscieng@gmail.com>
2021-03-11 18:04:24 +0100

    auth service

---
+++ auth-service/src/main/java/com/piggymetrics/auth/service/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.piggymetrics.auth.service;
+
+import com.piggymetrics.auth.domain.User;
+import com.piggymetrics.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public void create(User user) {
+
+		User existing = repository.findByUsername(user.getUsername());
+		Assert.isNull(existing, "user already exists: " + user.getUsername());
+
+		// TODO
+		// String hash = encoder.encode(user.getPassword());
+		// user.setPassword(hash);
+
+		repository.save(user);
+	}
+}
