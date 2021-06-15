b5655a84129d70024f929bda98649d9421eefebb
Tobi Lukan <ztobscieng@gmail.com>
2021-06-15 18:18:43 +0200

    fix unit tests

--- statistics-service/src/test/java/com/piggymetrics/statistics/client/ExchangeRatesClientTest.java
+++ statistics-service/src/test/java/com/piggymetrics/statistics/client/ExchangeRatesClientTest.java
@@ -1,21 +1,21 @@
 package com.piggymetrics.statistics.client;
 
-import com.piggymetrics.statistics.StatisticsApplication;
 import com.piggymetrics.statistics.domain.Currency;
 import com.piggymetrics.statistics.domain.ExchangeRatesContainer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDate;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = StatisticsApplication.class)
+@SpringBootTest
 public class ExchangeRatesClientTest {
 
 	@Autowired
@@ -29,8 +29,25 @@ public class ExchangeRatesClientTest {
 		assertEquals(container.getDate(), LocalDate.now());
 		assertEquals(container.getBase(), Currency.getBase());
 
+		assertNotNull(container.getRates());
+		assertEquals(Currency.values().length, container.getRates().size());
+		assertNotNull(container.getRates().get(Currency.USD.name()));
 		assertNotNull(container.getRates().get(Currency.EUR.name()));
 		assertNotNull(container.getRates().get(Currency.RUB.name()));
+	}
+
+	@Test
+	public void shouldRetrieveExchangeRatesForSpecifiedCurrency() {
+
+		Currency requestedCurrency = Currency.EUR;
+		ExchangeRatesContainer container = client.getRates(Currency.getBase(), Collections.singleton(requestedCurrency));
+
+		assertEquals(container.getDate(), LocalDate.now());
+		assertEquals(container.getBase(), Currency.getBase());
+
+		assertNotNull(container.getRates());
+		assertEquals(1, container.getRates().size());
+		assertNotNull(container.getRates().get(requestedCurrency.name()));
 	}
 
 }
